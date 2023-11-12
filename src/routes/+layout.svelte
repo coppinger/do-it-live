9be05eaa37036b6f9b70152a826d152a93aac5a4
coppinger <charles.coppinger@gmail.com>
2023-11-12 20:24:14 +1300

    nuke prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
       import "../app.css";
 </script>
 
-<slot/>
+<div class="bg-slate-950 text-slate-50 min-h-screen min-w-screen flex justify-center">
+    <div class="max-w-screen-2xl">
+        <slot/>
+    </div>
+</div>
