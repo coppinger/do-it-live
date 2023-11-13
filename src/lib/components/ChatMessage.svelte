ef87363a748e9f0fc51277d2b9a07c8638edec77
Charlie Coppinger <charles.coppinger@gmail.com>
2023-11-14 11:32:14 +1300

    fix?

---
+++ src/lib/components/ChatMessage.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { fly } from "svelte/transition";
+    import { quintOut } from "svelte/easing";
+
+    export let username;
+</script>
+
+<div
+    class="flex flex-col gap-2"
+    transition:fly={{
+        delay: 100,
+        duration: 500,
+        x: -200,
+        y: 0,
+        opacity: 0.5,
+        easing: quintOut,
+    }}
+>
+    <div class="flex gap-2 items-center">
+        <div class="h-8 w-8">
+            <img src={`/images/${username}.png`} alt="" />
+        </div>
+        <div class="flex">
+            <p class="font-bold">{username}</p>
+        </div>
+    </div>
+    <p class="font-medium"><slot /></p>
+</div>
---
+++ src/lib/components/Notification.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { fly } from "svelte/transition";
+    import { quintOut } from "svelte/easing";
+
+    export let type;
+    export let username;
+</script>
+
+<div
+    class="flex gap-4"
+    transition:fly={{
+        delay: 100,
+        duration: 500,
+        x: 200,
+        y: 0,
+        opacity: 0.5,
+        easing: quintOut,
+    }}
+>
+    <p class="order-2 font-bold">{username}</p>
+    {#if type === "follow"}
+        <p class="order-1">💖</p>
+        <p class="order-3">followed you</p>
+    {:else if type === "subscribe"}
+        <p class="order-1">🌟</p>
+        <p class="order-3">subscribed</p>
+    {:else if type === "raid"}
+        <p class="order-1">⚔️</p>
+        <p class="order-3">gave you a shoutout</p>
+    {:else if type === "shoutout"}
+        <p class="order-1">📢</p>
+        <p class="order-3">raided you with 1,337 ppl</p>
+    {/if}
+</div>
